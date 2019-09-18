117f8f9262ad0b403508845f74b603d24338fa51
Neil Campbell <neildcampbell@hotmail.com>
2019-09-18 16:10:57 +0100

    planes can be instructed to land and takeoff

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -1,1 +1,22 @@
 'use strict';
+
+describe('Feature Tests:', function(){
+var plane;
+var airport;
+
+beforeEach(function(){
+  plane = new Plane();
+  airport = new Airport();
+})
+
+it('planes can be instructed to land at an airport', function(){
+  plane.land(airport);
+  expect(airport.planes()).toContain(plane);
+});
+
+  it('planes can be insructed to take off', function(){
+    plane.land(airport)
+    plane.takeOff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+});
